ebb8105447bc1017a2612f199b0b683c7b9c0c63
ziad masalma <z.j.masalma@gmail.com>
2023-06-22 13:45:45 +0300

    z

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.assignment1;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.internal.ContextUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
     Questions q1;
     Questions q2;
     Questions q3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         question1=findViewById(R.id.textView3);
         question2=findViewById(R.id.textView4);
